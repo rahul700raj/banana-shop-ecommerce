dba090b172d4eb4d54af97f6bf242a4968cdcf86
rahul700raj <rm2778643@gmail.com>
2026-01-09 12:34:40 +0530

    Add WishlistContext - Wishlist state management

---
+++ src/context/WishlistContext.js
@@ -0,0 +1,63 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const WishlistContext = createContext();
+
+export const useWishlist = () => {
+  const context = useContext(WishlistContext);
+  if (!context) {
+    throw new Error('useWishlist must be used within WishlistProvider');
+  }
+  return context;
+};
+
+export const WishlistProvider = ({ children }) => {
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    const savedWishlist = localStorage.getItem('wishlist');
+    if (savedWishlist) {
+      setWishlist(JSON.parse(savedWishlist));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+  }, [wishlist]);
+
+  const addToWishlist = (product) => {
+    setWishlist((prevWishlist) => {
+      if (!prevWishlist.find((item) => item.id === product.id)) {
+        return [...prevWishlist, product];
+      }
+      return prevWishlist;
+    });
+  };
+
+  const removeFromWishlist = (productId) => {
+    setWishlist((prevWishlist) =>
+      prevWishlist.filter((item) => item.id !== productId)
+    );
+  };
+
+  const isInWishlist = (productId) => {
+    return wishlist.some((item) => item.id === productId);
+  };
+
+  const clearWishlist = () => {
+    setWishlist([]);
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{
+        wishlist,
+        addToWishlist,
+        removeFromWishlist,
+        isInWishlist,
+        clearWishlist,
+      }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+};
